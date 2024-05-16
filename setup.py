46634cb62b8fa7b48216ee25241c4c6d6028fb6c
Talha Rehman <talharehmanabid1999@gmail.com>
2024-05-16 22:07:40 +0300

    Setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    returns the requirements(the libaries to install)
+    '''
+    requirements = []
+    HYPHEN_E_DOT = '-e .'
+    with open(file_path)as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n','') for req in requirements]
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    return requirements
+
+
+setup(
+    name = 'MLProject',
+    version = '0.0.1',
+    author = 'Talha',
+    author_email = 'rehmantalha.yt@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+)
